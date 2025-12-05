ea2dc8d3a133c0aa46c8b17c4f4a49e3a52a8977
maximilianogonzalezcalzada <maxgonzalezcalzada@gmail.com>
2025-12-05 13:55:23 -0600

    refactor: delete numerous JPA entities, repositories, controllers, DTOs, and services across multiple modules, and update welcome templates.

--- src/main/java/mx/ipn/upiicsa/web/accesscontrol/jpa/repository/RolJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.ipn.upiicsa.web.accesscontrol.jpa.repository;
-
-import mx.ipn.upiicsa.web.controlacceso.external.jpa.model.RolJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolJpaRepository extends JpaRepository<RolJpa,Integer> {
-}
--- src/main/java/mx/ipn/upiicsa/web/hresources/jpa/repository/EstablecimientoJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.ipn.upiicsa.web.hresources.jpa.repository;
-
-import mx.ipn.upiicsa.web.hresources.jpa.model.EstablecimientoJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstablecimientoJpaRepository extends JpaRepository<EstablecimientoJpa,Integer> {
-}
