9d31dbf2361ec2029f95caa69d8f8ec5ac70bb8c
maximilianogonzalezcalzada <maxgonzalezcalzada@gmail.com>
2025-12-05 21:12:36 -0600

    feat: Display employee full names and branch names in the list and update work day list to use generic ID.

--- src/main/java/mx/ipn/upiicsa/web/hresources/domain/EmpleadoJpa.java
+++ src/main/java/mx/ipn/upiicsa/web/hresources/domain/EmpleadoJpa.java
@@ -12,9 +12,45 @@ public class EmpleadoJpa {
     @Column(name = "fk_id_sucursal")
     private Integer fkIdSucursal;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_empleado", referencedColumnName = "id_persona", insertable = false, updatable = false)
+    private mx.ipn.upiicsa.web.accesscontrol.infrastructure.adapter.out.persistence.model.PersonaJpa persona;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_id_sucursal", insertable = false, updatable = false)
+    private SucursalJpa sucursal;
+
     // getters/setters
-    public Integer getIdEmpleado() { return idEmpleado; }
-    public void setIdEmpleado(Integer idEmpleado) { this.idEmpleado = idEmpleado; }
-    public Integer getFkIdSucursal() { return fkIdSucursal; }
-    public void setFkIdSucursal(Integer fkIdSucursal) { this.fkIdSucursal = fkIdSucursal; }
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Integer getFkIdSucursal() {
+        return fkIdSucursal;
+    }
+
+    public void setFkIdSucursal(Integer fkIdSucursal) {
+        this.fkIdSucursal = fkIdSucursal;
+    }
+
+    public mx.ipn.upiicsa.web.accesscontrol.infrastructure.adapter.out.persistence.model.PersonaJpa getPersona() {
+        return persona;
+    }
+
+    public void setPersona(
+            mx.ipn.upiicsa.web.accesscontrol.infrastructure.adapter.out.persistence.model.PersonaJpa persona) {
+        this.persona = persona;
+    }
+
+    public SucursalJpa getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(SucursalJpa sucursal) {
+        this.sucursal = sucursal;
+    }
 }
